75e7783af295580e65ac03e1ec3e117feb84a7bb
Nildo Júnior <nildo.jnr@gmail.com>
2016-06-29 11:38:00 -0300

    Added a new spritesheet for the robot

--- src/util/animator/element_factories.js
+++ src/util/animator/element_factories.js
@@ -12,7 +12,7 @@ var animator = require("../animator");
 // If none are given, the image's original size is used.
 function createRobot(id, max_width, max_height) {
   var image = new Image();
-  image.src = "/static/images/elements/stormtrooper.png";
+  image.src = "/static/images/elements/robot.png";
   var IMAGE_WIDTH = 32;
   var IMAGE_HEIGHT = 48;
   var width = IMAGE_WIDTH;
@@ -34,7 +34,7 @@ function createRobot(id, max_width, max_height) {
   var walk_down_frames =  [0, 1, 2, 3];
   var walk_left_frames =  [4, 5, 6, 7];
   var walk_right_frames = [8, 9, 10, 11];
-  var walk_up_frames =    [12, 13, 14, 15];
+  // var walk_up_frames =    [12, 13, 14, 15];
 
   return new animator.AnimatedImageElement(
       id,
@@ -43,10 +43,10 @@ function createRobot(id, max_width, max_height) {
         new animator.SpriteAnimation("walk_down", walk_down_frames),
         new animator.SpriteAnimation("walk_left", walk_left_frames),
         new animator.SpriteAnimation("walk_right", walk_right_frames),
-        new animator.SpriteAnimation("walk_up", walk_up_frames),
+        // new animator.SpriteAnimation("walk_up", walk_up_frames),
       ],
       4,
-      4,
+      3,
       width,
       height
       );
